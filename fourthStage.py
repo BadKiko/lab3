758f388a5f2109c0226c4ec044b4ebdc7cd988eb
【﻿Ｋｉｋｏ】 <53761278+BadKiko@users.noreply.github.com>
2023-05-20 01:40:34 +0300

    Сделал начальные скрипты

---
+++ fourthStage.py
@@ -0,0 +1,118 @@
+import os
+import shapefile
+import json
+import csv
+import sys
+import requests
+import zipfile
+from tqdm import tqdm
+
+def downloadCoastline():
+    # URL для скачивания файла
+    url = "https://naciscdn.org/naturalearth/10m/cultural/ne_10m_admin_0_countries.zip"
+
+    # Папка для сохранения файлов
+    folder = "countries"
+
+    # Создание папки, если ее нет
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    else:
+        # Очистка папки, если она уже существует
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+
+    # Загрузка файла
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 1024  # 1 KB
+    progress_bar = tqdm(total=total_size, unit="B", unit_scale=True)
+
+    with open(os.path.join(folder, "ne_10m_coastline.zip"), "wb") as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+
+    progress_bar.close()
+    print("Распаковка архива")
+    # Разархивирование файла
+    zip_path = os.path.join(folder, "ne_10m_coastline.zip")
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(folder)
+
+    # Удаление zip-архива
+    os.remove(zip_path)
+
+def shp_to_json(input_shp, output_json):
+    reader = shapefile.Reader(input_shp)
+    fields = reader.fields[1:]
+    field_names = [field[0] for field in fields]
+    data = []
+    for sr in reader.shapeRecords():
+        atr = dict(zip(field_names, sr.record))
+        geom = sr.shape.__geo_interface__
+        data.append({"properties": atr, "geometry": geom})
+
+    with open(output_json, 'w') as json_file:
+        json.dump(data, json_file)
+
+    print(f"Сконвертировано: {input_shp}")
+
+
+def json_to_csv(input_json, output_csv):
+    with open(input_json, 'r') as json_file:
+        data = json.load(json_file)
+
+    with open(output_csv, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+
+        # Write CSV header
+        header = ["NAME", "latitude", "longitude"]
+        writer.writerow(header)
+
+        # Write data rows
+        for item in data:
+            properties = item["properties"]
+            geometry = item["geometry"]
+            if geometry["type"] == "Polygon":
+                coordinates = geometry["coordinates"][0]
+                for coordinate in coordinates:
+                    writer.writerow([properties["NAME"], coordinate[1], coordinate[0]])
+            elif geometry["type"] == "MultiPolygon":
+                polygons = geometry["coordinates"]
+                for polygon in polygons:
+                    for coordinate in polygon[0]:
+                        writer.writerow([properties["NAME"], coordinate[1], coordinate[0]])
+
+
+downloadCoastline()
+
+# Input folder containing SHP files, output folder for JSON and CSV files
+input_folder = "countries"
+output_folder = "countries/output_convert"
+
+# Create the output folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Get a list of all SHP files in the input folder
+shp_files = [file for file in os.listdir(input_folder) if file.lower().endswith(".shp")]
+
+total_files = len(shp_files)
+processed_files = 0
+
+# Iterate through each SHP file and perform the conversion
+for shp_file in shp_files:
+    processed_files += 1
+    print(f"Конвиртируется в файл {processed_files}/{total_files}: {shp_file}")
+
+    # Construct the file paths for input and output files
+    input_shp = os.path.join(input_folder, shp_file)
+    output_json = os.path.join(output_folder, f"{os.path.splitext(shp_file)[0]}.json")
+    output_csv = os.path.join(output_folder, f"{os.path.splitext(shp_file)[0]}.csv")
+
+    # Perform the conversion
+    shp_to_json(input_shp, output_json)
+    json_to_csv(output_json, output_csv)
+
+print("Конвертация из CSV завершена.")
--- secondStage.py
+++ secondStage.py
@@ -12,9 +12,23 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import re
+import os
 
 link = "https://academic.udayton.edu/kissock/http/Weather/citylistWorld.htm"
 
+def makeFolder():
+    # Папка для сохранения файлов
+    folder = "data"
+
+    # Создание папки, если ее нет
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    else:
+        # Очистка папки, если она уже существует
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
 
 def find_dataset(text):
     pattern = r'\b\w+\.txt\b'
@@ -36,15 +50,19 @@ for item in city_list:
     city = item.text.partition('(')[0].strip()
     cities.append(city)
 
-    region = item.find_previous("p").text.strip()
+    region = item.find_previous("p").text.strip().replace("\n", " ")
     if region:
         regions.append(region)
 
     region_span = item.find_previous("span", style="mso-bidi-font-size:10.0pt;font-family:Arial")
+    if not region_span:
+        region_span = item.find_previous("span", style=re.compile(r".*color:maroon.*"))
     region = region_span.text if region_span else ""
     realRegions.append(region)
 
 data = []
+
+set_south_america_region = False  # Флаг для определения, когда установить регион "South/Central America & Carribean"
 
 for item in find:
     result = find_dataset(item.text)
@@ -54,21 +72,35 @@ for item in find:
     countries = regions[i]
 
     region_span = item.find("span", style="mso-bidi-font-size:10.0pt;font-family:Arial")
-    region = realRegions[i]
+    if not region_span:
+        region_span = item.find("span", style=re.compile(r".*color:maroon.*"))
+    region = realRegions[i].replace("\n", " ").replace("\r", "")
     country_spans = item.findAll("span", style=re.compile(r".*color:maroon.*"))
     for span in country_spans:
         country = re.sub(r'[\[\]\r\n]', '', span.text).strip()
         if country:
             countries += ", " + country
 
+    #Если пустой регион берем старые значения иначе иногда пустые поля идут
+    if region == "":
+        region = realRegions[i-1].replace("\n", " ").replace("\r", "")
+        realRegions[i]=region
+
+    # Установка значения "South/Central America & Carribean" для регионов после "Argentina" костыль так и не понял почему не берется
+    if "Argentina" in countries:
+        set_south_america_region = True
+    if set_south_america_region:
+        region = "South/Central America & Carribean"
+
     if region or countries or cities[i] or dataset:
         data.append([region, countries, cities[i], dataset])
 
     i += 1
 
+makeFolder()
 
 # Запись данных в файл CSV
-filename = "data.csv"
+filename = "data/data.csv"
 
 with open(filename, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
