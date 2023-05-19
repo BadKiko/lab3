7b752f282c6c756432d33babaed61d48c15bc074
【﻿Ｋｉｋｏ】 <53761278+BadKiko@users.noreply.github.com>
2023-05-20 01:45:05 +0300

    main update

--- fourthStage.py
+++ fourthStage.py
@@ -21,6 +21,8 @@ def downloadCoastline():
         # Очистка папки, если она уже существует
         for filename in os.listdir(folder):
             file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
 
     # Загрузка файла
     response = requests.get(url, stream=True)
---
+++ main.py
@@ -0,0 +1,7 @@
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("Lab-03")
+ascii_banner2 = pyfiglet.figlet_format("Maker")
+print(ascii_banner)
+print("\t\t\tCreated by Kiko with ♥")
+print(ascii_banner2)
